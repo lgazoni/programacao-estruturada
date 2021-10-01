952f6c8c9376da133b3552c2afbd9efe9d8ea7e1
Leandro Gazoni <lgazoni@coc.ufrj.br>
2021-10-01 20:28:15 -0300

    Enviando Versão Olá Mundo

---
+++ 01.HelloWorld/main.c
@@ -0,0 +1,16 @@
+/*
+Meu primeiro programa em C
+Imprimir uma mensagem na Tela
+*/
+
+#include <stdio.h>  // Declarações de bibliotecas utilizadas pelo C
+#include <stdlib.h> // Declarações de bibliotecas utilizadas pelo C
+
+int main() // funcao principal do programa, inicio!
+{
+   
+    printf("Hello World!\n"); // comando para imprimir uma msg na tela de saída
+
+    system("pause"); // faz uma pausa no programa antes de terminar
+    return 0;        // Retorna Zero, pois informa ao SO que foi executado sem problemas.
+} // fim do programa
